0b3914673ba6596c04ead2b3cb8710a1d7520e9f
julenka7 <londarenko.julia@gmail.com>
2022-06-20 14:24:57 +0300

    remove comments

--- tests/ylond/conftest.py
+++ tests/ylond/conftest.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 
 
 driver_path = os.environ.get("DRIVER_PATH", "chromedriver")
-#HOST = 'https://demoqa.com'
-#URL = "/text-box"
 
 
 @pytest.fixture(scope="session")
@@ -16,16 +14,3 @@ def session():
     yield session
     session.quit()
     time.sleep(2)
-
-
-
-#     driver = webdriver.Chrome(driver_path)
-#     #driver.get(HOST + URL)
-#     time.sleep(2)
-#     driver.maximize_window()
-#
-#     yield
-#     driver.close()
-#     time.sleep(1)
-#
-# def session():
