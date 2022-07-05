211e298fc1ad4596aa6025b23faa1080c895566d
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-07-05 15:16:14 +0300

    constant for firefox

--- tests/akaiafiuk/constants.py
+++ tests/akaiafiuk/constants.py
@@ -3,7 +3,7 @@ import os
 HOST = 'https://demoqa.com'
 LOGIN = os.environ['LOGIN']
 PASSWORD = os.environ['PASSWORD']
-DRIVER = os.environ['DRIVER']
+DRIVER = 'firefox'
 
 AUTOMATION_PRACTICE_HOST = 'http://automationpractice.com/'
 WAIT_TIME = 30
