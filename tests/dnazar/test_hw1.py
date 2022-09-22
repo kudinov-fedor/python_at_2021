4c8569ab594e185461f205d830505828e9737079
Diana <olkdi@outlook.com>
2022-09-22 18:55:30 +0300

    Create test_hw1

---
+++ tests/dnazar/test_hw1.py
@@ -0,0 +1,34 @@
+import random
+import pytest
+
+
+@pytest.fixture()
+def get_num():
+    return random.randint(-100, 100)
+
+
+def test_append(get_num):
+    my_list = [1, 2, 3]
+    my_list.append(get_num)
+    assert (get_num in my_list) is True
+
+
+@pytest.mark.skipif(get_num != 50, reason="Random returns always different numbers")
+def test_random_equals_fifty(get_num):
+    assert get_num == 50
+
+
+@pytest.mark.xfail
+def test_fail_hash():
+    my_first_str = 'Test'
+    my_second_str = 'Test '
+    assert hash(my_first_str) == hash(my_second_str)
+
+
+@pytest.mark.parametrize("param1, param2, res", [
+    (-13, 2, 1),
+    (0, 9, 0),
+    (7, 5, 2),
+])
+def test_module(param1, param2, res):
+    assert param1 % param2 == res
