2bfc9bfcdd979fa2888dd32647c9b7c74a6aea49
Monika Sonta <msont@softserveinc.com>
2022-10-24 10:02:37 +0200

    Create API client for Books Store App

---
+++ tests/msonta/api/helpers/api_client.py
@@ -0,0 +1,84 @@
+import os
+import requests
+
+
+class ApiClient:
+    host = "https://demoqa.com"
+
+    def __init__(self):
+        self.client = requests.session()
+        self.payload = {
+            "userName": os.environ["USER_NAME"],
+            "password": os.environ["PASSWORD"]
+        }
+
+    def create_user(self):
+        res = self.client.post(url=self.host + "/Account/v1/User",
+                               json=self.payload)
+        return res
+
+    def generate_token(self):
+        res = self.client.post(url=self.host + "/Account/v1/GenerateToken",
+                               json=self.payload)
+        return res
+
+    def login(self):
+        res = self.client.post(url=self.host + "/Account/v1/Login",
+                               json=self.payload)
+        return res
+
+    def check_authorization(self):
+        res = self.client.post(url=self.host + "/Account/v1/Authorized",
+                               json=self.payload)
+        return res
+
+    def get_user(self, token, user_id):
+        headers = {"Authorization": f"Bearer {token}"}
+        res = self.client.get(url=self.host + f"/Account/v1/User/{user_id}",
+                              headers=headers, json=self.payload)
+        return res
+
+    def get_books(self):
+        res = self.client.get(url=self.host + "/BookStore/v1/Books")
+        return res
+
+    def get_book(self, isbn):
+        query_params = {"ISBN": isbn}
+        res = self.client.get(url=self.host + f"/BookStore/v1/Book",
+                              params=query_params)
+        return res
+
+    def add_book_to_collection(self, isbn, token, user_id):
+        headers = {"Authorization": f"Bearer {token}"}
+        json = {"userId": f"{user_id}", "collectionOfIsbns": [
+            {"isbn": f"{isbn}"}]}
+        res = self.client.post(url=self.host + "/BookStore/v1/Books",
+                               headers=headers, json=json)
+        return res
+
+    def change_book_in_collection(self, isbn, token, user_id):
+        headers = {"Authorization": f"Bearer {token}"}
+        json = {"userId": f"{user_id}", "isbn": f"{isbn}"}
+        res = self.client.put(url=self.host + f"/BookStore/v1/Books/{isbn}",
+                              headers=headers, json=json)
+        return res
+
+    def delete_book(self, token, user_id, isbn):
+        headers = {"Authorization": f"Bearer {token}"}
+        json = {"userId": f"{user_id}", "isbn": f"{isbn}"}
+        res = self.client.delete(url=self.host + "/BookStore/v1/Book",
+                                 headers=headers, json=json)
+        return res
+
+    def delete_all_books(self, token, user_id):
+        headers = {"Authorization": f"Bearer {token}"}
+        query_params = {"UserId": user_id}
+        res = self.client.delete(url=self.host + f"/BookStore/v1/Books",
+                                 headers=headers, params=query_params)
+        return res
+
+    def delete_user(self, token, user_id):
+        headers = {"Authorization": f"Bearer {token}"}
+        res = self.client.delete(url=self.host + f"/Account/v1/User/{user_id}",
+                                 headers=headers)
+        return res
