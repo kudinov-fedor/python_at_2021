cead810aaeb963d7d326cac5e2e8fd278cc4781b
fkudinov <fkudinov@modeln.com>
2023-03-24 19:47:21 +0200

    add basics for phptraveler

---
+++ phptravels/constants.py
@@ -0,0 +1,18 @@
+host = "https://phptravels.net"
+
+admin_url = f"{host}/admin"
+admin_name = "admin@phptravels.com"
+admin_pass = "demoadmin"
+
+cust_url = f"{host}/login"
+cust_login = "user@phptravels.com"
+cust_pass = "demouser"
+
+
+agent_url = f"{host}/login"
+agent_login = "agent@phptravels.com"
+agent_pass = "demoagent"
+
+supplier_url = f"{host}/supplier"
+supplier_login = "supplier@phptravels.com"
+supplier_pass = "demosupplier"
