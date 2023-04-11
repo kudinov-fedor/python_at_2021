7d5fba9a39b43034a9f951d978443c5512689d23
Szymon Kloc <skloc@softserveinc.com>
2023-04-11 13:08:03 +0200

    fixed checkbox 2nd lvl test

--- tests/skloc/demoQA/Elements/test_checkboxes.py
+++ tests/skloc/demoQA/Elements/test_checkboxes.py
@@ -12,16 +12,37 @@ def test_setup():
     driver.close()
     driver.quit()
 
-def test_mark_all_collapsed():
 
+def test_mark_all_collapsed():
     top_check_btn = driver.find_element_by_css_selector(".rct-checkbox")
 
     top_check_btn.click()
 
     results = driver.find_elements_by_css_selector("#result span")
 
-    expectedMsg = "You have selected : home desktop notes commands documents workspace react angular veu office " \
-              "public private classified general downloads wordFile excelFile"
+    expected_msg = "You have selected : home desktop notes commands documents workspace react angular veu office " \
+                   "public private classified general downloads wordFile excelFile"
 
     for result in results:
-        assert (expectedMsg.__contains__(result.text) and result.is_displayed())
+        assert expected_msg.__contains__(result.text) and result.is_displayed()
+
+
+def test_mark_2nd_lvl_element():
+    home_drop_down_btn = driver.find_element_by_css_selector(".rct-collapse-btn")
+
+    home_drop_down_btn.click()
+
+    desktop_checkbox = driver.find_element_by_xpath("//label[@for='tree-node-desktop']/span[@class='rct-checkbox']")
+    desktop_checkbox.click()
+
+    results = driver.find_elements_by_css_selector("#result span")
+    expected_msg = "You have selected : desktop notes commands"
+    home_checkbox_mark = driver.find_element_by_xpath(
+        "//label[@for='tree-node-home']/span[@class='rct-checkbox']/*").get_attribute("class")
+    desktop_checkbox_mark = driver.find_element_by_xpath(
+        "//label[@for='tree-node-desktop']/span[@class='rct-checkbox']/*").get_attribute("class")
+
+    assert "rct-icon-half-check" in home_checkbox_mark
+    assert "rct-icon-check" in desktop_checkbox_mark
+    for result in results:
+        assert expected_msg.__contains__(result.text) and result.is_displayed()
