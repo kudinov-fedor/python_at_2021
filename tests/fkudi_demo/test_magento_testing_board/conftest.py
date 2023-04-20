adedcda78abe173d5fc12e7064dc01c25cfd789d
fkudinov <fkudinov@modeln.com>
2023-04-20 23:41:53 +0300

    Segregate user

--- tests/fkudi_demo/test_magento_testing_board/conftest.py
+++ tests/fkudi_demo/test_magento_testing_board/conftest.py
@@ -2,6 +2,15 @@ import pytest
 
 from selenium.webdriver import Chrome
 
+from magento_softwaretesting_board import config
+
+
+@pytest.fixture
+def user():
+    return {"login": config.LOGIN,
+            "password": config.PASSWORD,
+            "name": config.USER_NAME}
+
 
 @pytest.fixture
 def session():
