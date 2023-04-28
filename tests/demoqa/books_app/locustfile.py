215e4680575899a241d6277d4b3c54f1a446bda6
fkudinov <fkudinov@modeln.com>
2023-04-28 14:01:52 +0300

    small change in locust file

--- tests/demoqa/books_app/locustfile.py
+++ tests/demoqa/books_app/locustfile.py
@@ -1,12 +1,19 @@
+import random
 from locust import HttpUser, between, task
 
 from api_client_locust_compatible import ApiClient
+
+
+# locust -f /Users/fkudi/PycharmProjects/python_at_2021/tests/demoqa/books_app/locustfile.py
+# -H https://demoqa.com -u 1 -r 5 -t 300s --autostart -P 8089
 
 
 class LocustClient(HttpUser, ApiClient):
     wait_time = between(1, 5)
 
     def on_start(self):
+        self.login = f"test_{random.randint(0, 999999)}"
+        self.password = "Test123!@#"
         self.create()
 
     def on_stop(self):
