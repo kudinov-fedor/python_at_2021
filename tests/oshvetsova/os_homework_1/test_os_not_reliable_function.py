e651bb60b027aa13c883d5e3cb2c308d56f67334
oshvetsova <oshvetsova@atlassian.com>
2024-04-11 22:52:36 +0300

    HW1

---
+++ tests/oshvetsova/os_homework_1/test_os_not_reliable_function.py
@@ -0,0 +1,14 @@
+"""
+Cover with tests
+"""
+
+import random
+
+
+
+def not_reliable():
+    res = random.random()
+    if res < 0.5:
+        raise RuntimeError("Res is less than 0.5:  {}".format(res))
+    return res
+
