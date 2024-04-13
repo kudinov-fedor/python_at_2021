a40bce3256cbe9fa01d09f946de72c758f169eba
t_hrechkina <t_hrechkina-itssv@kw.com>
2024-04-13 12:14:54 +0300

    test for config&not_reliable functions

--- tests/threc/hw_password/test_password.py
+++ tests/threc/hw_password/test_password.py
@@ -10,7 +10,7 @@ from tests.threc.hw_password.check_password import check_password
 def test_password_positive(res):
     assert check_password(res)
 
-@pytest.mark.negative('Password with one symbol')
+@pytest.mark.negative()
 @pytest.mark.parametrize(["res"], [
     pytest.param('1', id='Password with one symbol'),
     pytest.param(' ', id='Password with space'),
@@ -18,13 +18,13 @@ def test_password_positive(res):
     pytest.param('Q8@e', id='Password with less then 8 symbols'),
     pytest.param('qwertyuiop', id='Password with small letters'),
     pytest.param('12345678', id='Password with only numbers'),
-    pytest.param('qwerty8@', id='Password without capital letter, but with nuber and special character'),
-    pytest.param('qwerty88', id='Password without capital letter and special character, but with number'),
-    pytest.param('qwerty@@', id='Password without capital letter and number, but with special character'),
-    pytest.param('Qwerty88', id='Password without special characted, but with capital letter and number'),
-    pytest.param('Qwerty@@', id='Password without number, but with capital letter and special symbol'),
-    pytest.param('Qwe8@', id='Password with capital letter, special symbol, number, but less than 8 symbols'),
-    pytest.param('Qwertyuiopasdfghjklzxcvbnmqw8@', id='Password with more that 30 symbols'),
+    pytest.param('qwerty8@', id='Password with lower letter, number and special character'),
+    pytest.param('qwerty88', id='Password with lower letters and numbers'),
+    pytest.param('qwerty@@', id='Password with lower letters, special character'),
+    pytest.param('Qwerty88', id='Password capital letter and number'),
+    pytest.param('Qwerty@@', id='Password with capital letter and special symbol'),
+    pytest.param('Qwe8@', id='Length for password is too short'),
+    pytest.param('Qwertyuiopasdfghjklzxcvbnmqw8@', id='Too long password'),
     pytest.param("string with 123@", id='Password with several words')
 ])
 
