d2c273297b8a98a6eb18e53b2be0c9988a66d7c8
oshvetsova <oshvetsova@atlassian.com>
2024-04-14 16:27:44 +0300

    HW1

--- tests/oshvetsova/os_homework_1/test_os_not_reliable_function.py
+++ tests/oshvetsova/os_homework_1/test_os_not_reliable_function.py
@@ -1,9 +1,11 @@
+import pytest
+
+
 """
 Cover with tests
 """
 
 import random
-
 
 
 def not_reliable():
@@ -12,3 +14,18 @@ def not_reliable():
         raise RuntimeError("Res is less than 0.5:  {}".format(res))
     return res
 
+
+def test_less_than_0_5(mocker):
+    mocker.patch.object(random, "random", return_value=0.4)
+    with pytest.raises(RuntimeError):
+        not_reliable()
+
+
+def test_more_than_0_5(mocker):
+    mocker.patch.object(random, "random", return_value=0.6)
+    assert not_reliable() == 0.6
+
+
+def test_equals_to_0_5(mocker):
+    mocker.patch.object(random, "random", return_value=0.5)
+    assert not_reliable() == 0.5
---
+++ tests/oshvetsova/os_homework_1/test_os_not_reliable_function_2.py
@@ -0,0 +1,22 @@
+"""
+Cover with tests
+"""
+
+import pytest
+from tests.oshvetsova.os_homework_1.not_reliable_function_2 import not_reliable
+
+
+def test_less_than_0_5(mocker):
+    mocker.patch('tests.oshvetsova.os_homework_1.not_reliable_function_2.random', return_value=0.4)
+    with pytest.raises(RuntimeError, match=r"Res is less than 0.5:  0.4"):
+        not_reliable()
+
+
+def test_equal_to_0_5(mocker):
+    mocker.patch('tests.oshvetsova.os_homework_1.not_reliable_function_2.random', return_value=0.5)
+    assert not_reliable() == 0.5
+
+
+def test_greater_to_0_5(mocker):
+    mocker.patch('tests.oshvetsova.os_homework_1.not_reliable_function_2.random', return_value=0.6)
+    assert not_reliable() == 0.6
