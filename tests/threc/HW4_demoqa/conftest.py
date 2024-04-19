f15dbec4abaedf337ad53d81a4b7b1d234b5751a
t_hrechkina <t_hrechkina-itssv@kw.com>
2024-04-19 19:24:20 +0300

    update tests for demoqa/text-box

--- tests/threc/HW4_demoqa/conftest.py
+++ tests/threc/HW4_demoqa/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from selenium.webdriver.common.by import By
+
 import constants
 
 from selenium.webdriver import Chrome
@@ -8,6 +10,14 @@ from selenium.webdriver import Chrome
 def driver():
     driver = Chrome()
     driver.get(constants.URL_HOST)
+
+    driver.find_element(By.ID, 'userName').send_keys(constants.FIRST_NAME)
+    driver.find_element(By.ID, 'userEmail').send_keys(constants.USER_EMAIL)
+    driver.find_element(By.ID, 'currentAddress').send_keys(constants.CURRENT_ADDRESS)
+    driver.find_element(By.ID, 'permanentAddress').send_keys(constants.PERMANENT_ADDRESS)
+
+    driver.implicitly_wait(3)
+
     yield driver
 
     # tear down
--- tests/threc/HW4_demoqa/test_textbox.py
+++ tests/threc/HW4_demoqa/test_textbox.py
@@ -5,13 +5,6 @@ import constants
 
 
 def test_textbox(driver):
-    driver.find_element(By.ID, 'userName').send_keys(constants.FIRST_NAME)
-    driver.find_element(By.ID, 'userEmail').send_keys(constants.USER_EMAIL)
-    driver.find_element(By.ID, 'currentAddress').send_keys(constants.CURRENT_ADDRESS)
-    driver.find_element(By.ID, 'permanentAddress').send_keys(constants.PERMANENT_ADDRESS)
-
-    driver.implicitly_wait(3)
-
     scroll_origin = ScrollOrigin.from_viewport(10, 10)
 
     ActionChains(driver)\
@@ -21,13 +14,13 @@ def test_textbox(driver):
     driver.find_element(By.ID, 'submit').click()
 
     first_name = driver.find_element(By.ID, 'name').text
-    just_first_name = first_name.lstrip('Name:')
+    just_first_name = first_name.removeprefix('Name:')
     assert just_first_name == constants.FIRST_NAME
 
     user_email = driver.find_element(By.ID, 'email').text
-    just_email = user_email.lstrip('Email:')
+    just_email = user_email.removeprefix('Email:')
     assert just_email in constants.USER_EMAIL
 
     permanent_address = driver.find_element(By.ID, 'permanentAddress').text
-    just_permanent_address = permanent_address.lstrip('Permananet Address :')
+    just_permanent_address = permanent_address.removeprefix('Permananet Address :')
     assert just_permanent_address in constants.PERMANENT_ADDRESS
