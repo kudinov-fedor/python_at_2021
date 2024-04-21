eeddfe1edd17ad041b055c82270ad2852db6890b
vvashchu <vvashchu@cisco.com>
2024-04-21 18:12:09 +0300

    changed_locators

---
+++ tests/vvashchu/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium.webdriver import Chrome
+from tests.vvashchu.locators import LoginPage
+from tests.vvashchu.locators import ProductPage
+
+
+HOST = "https://www.saucedemo.com"
+LOGIN = "standard_user"
+PASSORD = "secret_sauce"
+
+
+@pytest.fixture
+def session():
+    session = Chrome()
+    yield session
+    session.quit()
+
+
+@pytest.fixture(autouse=True)
+def login(session):
+    session.get(HOST)
+
+    # вхід в систему
+    session.find_element(*LoginPage.UserName).send_keys(LOGIN)
+    session.find_element(*LoginPage.Password).send_keys(PASSORD)
+    session.find_element(*LoginPage.LoginBtn).click()
+
+
+@pytest.fixture
+@pytest.mark.usefixtures("login")
+def cart_with_2_items(session):
+
+    elements = session.find_elements(*ProductPage.Elements)
+    assert len(elements) == 6
+
+    elements[0].find_element(*ProductPage.Element).click()
+    elements[2].find_element(*ProductPage.Element).click()
