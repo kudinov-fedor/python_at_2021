2d40f251a94b527b4cca1685829ab224d22d363f
Sofiiar <s_radul-itssv@kw.com>
2024-04-21 13:11:55 +0200

    SR-1: updated tests

--- tests/sradu/homework_1/tests/test_not_reliable_function_2.py
+++ tests/sradu/homework_1/tests/test_not_reliable_function_2.py
@@ -1,14 +1,14 @@
 import pytest
-from tests.sradu.homework_1.not_reliable_function_2 import not_reliable
+import tests.sradu.homework_1.not_reliable_function_2 as module
 
 
 def test_not_reliable_success(mocker):
-    mocker.patch('tests.sradu.homework_1.not_reliable_function_2.random', return_value=0.5)
-    result = not_reliable()
+    mocker.patch.object(module, 'random', return_value=0.5)
+    result = module.not_reliable()
     assert result == 0.5
 
 
 def test_not_reliable_failure(mocker):
-    mocker.patch('tests.sradu.homework_1.not_reliable_function_2.random', return_value=0.49)
+    mocker.patch.object(module, 'random', return_value=0.49)
     with pytest.raises(RuntimeError, match=r"Res is less than 0.5:\s+0.49"):
-        not_reliable()
+        module.not_reliable()
--- tests/sradu/homework_1/tests/test_not_reliable_function_3.py
+++ tests/sradu/homework_1/tests/test_not_reliable_function_3.py
@@ -1,24 +1,24 @@
 import pytest
-from tests.sradu.homework_1.not_reliable_function_3 import not_reliable
+import tests.sradu.homework_1.not_reliable_function_3 as module
 
 
 def test_not_reliable_success_first_try(mocker):
-    mocker.patch('tests.sradu.homework_1.not_reliable_function_3.random', return_value=0.9)
-    result, tries = not_reliable()
+    mocker.patch.object(module, 'random', return_value=0.9)
+    result, tries = module.not_reliable()
     assert result >= 0.888
     assert  tries == 1
 
 
 def test_not_reliable_success_maximum_tries(mocker):
     sequence_of_returns = [0.1, 0.887, 0.0, 0.01, 0.1, 0.887, 0.0, 0.01, 0.01, 0.888]
-    mocker.patch('tests.sradu.homework_1.not_reliable_function_3.random', side_effect=sequence_of_returns)
-    result, tries = not_reliable()
+    mocker.patch.object(module, 'random', side_effect=sequence_of_returns)
+    result, tries = module.not_reliable()
     assert result >= 0.888
     assert tries == 10
 
 
 def test_not_reliable_failure(mocker):
     sequence_of_returns = [0.1] * 11
-    mocker.patch('tests.sradu.homework_1.not_reliable_function_3.random', side_effect=sequence_of_returns)
+    mocker.patch.object(module, 'random', side_effect=sequence_of_returns)
     with pytest.raises(RuntimeError, match="Its too much, total tries: 11"):
-        not_reliable()
+        module.not_reliable()
