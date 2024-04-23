501eaa2f71641c98863fcc8f44edd3d56abfe3d1
Sofiiar <s_radul-itssv@kw.com>
2024-04-23 19:46:42 +0200

    SR-2: fixed issues for tests

--- tests/sradu/homework_2/test_saucedemo.py
+++ tests/sradu/homework_2/test_saucedemo.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-
 HOST = "https://www.saucedemo.com"
 LOGIN = "standard_user"
 PASSWORD = "secret_sauce"
@@ -13,10 +12,10 @@ PASSWORD = "secret_sauce"
 
 @pytest.fixture(scope="function")
 def session():
-    session = Chrome()
-    session.get(HOST)
-    yield session
-    session.quit()
+    driver = Chrome()
+    driver.get(HOST)
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture(scope="function")
@@ -24,10 +23,10 @@ def login(session):
     session.find_element(By.ID, "user-name").send_keys(LOGIN)
     session.find_element(By.ID, "password").send_keys(PASSWORD)
     session.find_element(By.ID, "login-button").click()
-    return session
 
 
-def test_add_products_to_cart_and_checkout(session, login):
+@pytest.mark.usefixtures("login")
+def test_add_products_to_cart_and_checkout(session):
     elements = session.find_elements(By.CSS_SELECTOR, ".inventory_list .inventory_item")
     assert len(elements) == 6, "Expected 6 inventory items"
 
@@ -44,7 +43,7 @@ def test_add_products_to_cart_and_checkout(session, login):
     session.find_element(By.ID, "shopping_cart_container").click()
 
     # перевірити кількість елементів в корзині
-    items = login.find_elements(By.CSS_SELECTOR, ".cart_list .cart_item")
+    items = session.find_elements(By.CSS_SELECTOR, ".cart_list .cart_item")
     assert len(items) == 2, "Expected 2 items in the cart checkout"
 
     # перейти до оформлення замовлення
@@ -64,16 +63,15 @@ def test_add_products_to_cart_and_checkout(session, login):
         cart.find_element(By.CLASS_NAME, "shopping_cart_badge")
 
 
-""" перевірити кількість і назви menu items
-1 - клікнути по burger menu button
-2 - перевірити кількість items
-3 - перевірити порядок найменувань items
-"""
-
-
-def test_menu_items(session, login):
-    WebDriverWait(session, 10).until(EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))).click()
-    WebDriverWait(session, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".bm-menu a[id]")))
+@pytest.mark.usefixtures("login")
+def test_menu_items(session):
+    """ перевірити кількість і назви menu items
+    1 - клікнути по burger menu button
+    2 - перевірити кількість items
+    3 - перевірити порядок найменувань items
+    """
+    WebDriverWait(session, 5).until(EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))).click()
+    WebDriverWait(session, 5).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".bm-menu a[id]")))
     size = len(session.find_elements(By.CSS_SELECTOR, ".bm-menu a[id]"))
     assert size == 4
 
@@ -85,28 +83,25 @@ def test_menu_items(session, login):
         assert actual_link.text == expected_link_text, f"Expected '{expected_link_text}', but got '{actual_link.text}'"
 
 
-"""перевірити сортування в порядку зростанння
-1 - клікнути по dropdown сортування
-2 - клікнути Price (low to high) 
-3 - перевірити, що ціни на картках з товарами йдуть у зростаючому порядку
-"""
+@pytest.mark.usefixtures("login")
+def test_prices_in_ascending_order(session):
+    """перевірити сортування в порядку зростанння
+    1 - клікнути по dropdown сортування
+    2 - клікнути Price (low to high)
+    3 - перевірити, що ціни на картках з товарами йдуть у зростаючому порядку
+    """
+    wait = WebDriverWait(session, 5)
 
+    checks = [EC.element_to_be_clickable((By.CSS_SELECTOR, "select[data-test='product-sort-container']")),
+              EC.element_to_be_clickable((By.XPATH, "//option[text()='Price (low to high)']"))
+              ]
 
-def test_prices_in_ascending_order(session, login):
-    WebDriverWait(session, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "select[data-test='product-sort-container']"))
-    ).click()
+    for check in checks:
+        wait.until(check).click()
 
-    WebDriverWait(session, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//option[text()='Price (low to high)']"))
-    ).click()
-
-    WebDriverWait(session, 10).until(
-        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[data-test='inventory-item-price']")))
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[data-test='inventory-item-price']")))
 
     price_elements = session.find_elements(By.CSS_SELECTOR, "div[data-test='inventory-item-price']")
     prices_values = [float(price_element.text.replace('$', '')) for price_element in price_elements]
 
-    print(f"Expected prices: {prices_values}")
     assert prices_values == sorted(prices_values), f"Prices are not in the ascending order: {prices_values}"
-
