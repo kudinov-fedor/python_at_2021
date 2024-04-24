b09fb88af44cba7d66275c6cb26b9a2cdd09acc0
Sofiiar <s_radul-itssv@kw.com>
2024-04-24 12:02:06 +0200

    SR-2: fixed issues for tests

--- tests/sradu/homework_2/test_saucedemo.py
+++ tests/sradu/homework_2/test_saucedemo.py
@@ -1,26 +1,17 @@
 import pytest
-from typing import List
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
+
+from tests.sradu.homework_2.utils import wait_and_click, wait_until_all_elements_visible
 
 HOST = "https://www.saucedemo.com"
 LOGIN = "standard_user"
 PASSWORD = "secret_sauce"
 
 
-def wait_and_click(session, by, locator):
-    WebDriverWait(session, 5).until(EC.element_to_be_clickable((by, locator))).click()
-
-
-def wait_until_all_elements_visible(session, by, locator) -> List[WebElement]:
-    return WebDriverWait(session, 5).until(EC.visibility_of_all_elements_located((by, locator)))
-
-
-@pytest.fixture(scope="function")
+@pytest.fixture
 def session():
     driver = Chrome()
     driver.get(HOST)
@@ -28,7 +19,7 @@ def session():
     driver.quit()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def login(session):
     session.find_element(By.ID, "user-name").send_keys(LOGIN)
     session.find_element(By.ID, "password").send_keys(PASSWORD)
