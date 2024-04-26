507801fbcbfc11c13d71dbb72a1f42130edf0d7f
Yevhen Semenov <eugene.semenoff@gmail.com>
2024-04-26 23:57:18 +0300

    homework 3 (locators)

--- tests/ysemenov/hw2/test_sauce_orders.py
+++ tests/ysemenov/hw2/test_sauce_orders.py
@@ -1,6 +1,6 @@
 from selenium.webdriver import Chrome
 from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
+from tests.ysemenov.hw3.locators import *
 import pytest
 
 HOST = "https://www.saucedemo.com"
@@ -31,36 +31,36 @@ def test_order_product(session):
     session.get(HOST)
 
     # 1. Log into the web store
-    session.find_element(By.ID, "user-name").send_keys(LOGIN)
-    session.find_element(By.ID, "password").send_keys(PASSWORD)
-    session.find_element(By.ID, "login-button").click()
+    session.find_element(*LoginPage.txtLogin).send_keys(LOGIN)
+    session.find_element(*LoginPage.txtPassword).send_keys(PASSWORD)
+    session.find_element(*LoginPage.btnSubmit).click()
 
-    # 2. Add one product to cart
-    elements = session.find_elements(By.CSS_SELECTOR, ".inventory_list .inventory_item")
-    elements[0].find_element(By.XPATH, ".//*[@class='pricebar']//button").click()
+    # 2. Add one product to cart in the Inventory page
+    elements = session.find_elements(*InventoryPage.lstInventory)
+    elements[0].find_element(*InventoryPage.btnAddToCart).click()
 
-    # 3. Open cart
-    cart = session.find_element(By.ID, "shopping_cart_container")
+    # 3. Open cart by clicking Cart button
+    cart = session.find_element(*HeaderMenu.btnCart)
     cart.click()
 
     # 4. Verify number of items added is 1
-    items = session.find_elements(By.CSS_SELECTOR, ".cart_list .cart_item")
+    items = session.find_elements(*CartPage.lstCartItems)
     assert len(items) == 1
 
-    # 5. Navigate to Checkout
-    session.find_element(By.ID, "checkout").click()
+    # 5. Click Checkout to navigate to Checkout page
+    session.find_element(*CartPage.btnCheckout).click()
 
     # 6. Fill in details and click Continue
-    session.find_element(By.ID, "first-name").send_keys("Luke")
-    session.find_element(By.ID, "last-name").send_keys("Skywalker")
-    session.find_element(By.ID, "postal-code").send_keys("90210")
-    session.find_element(By.ID, "continue").click()
+    session.find_element(*CheckoutPage.txtFirstName).send_keys("Luke")
+    session.find_element(*CheckoutPage.txtLastName).send_keys("Skywalker")
+    session.find_element(*CheckoutPage.txtPostalCode).send_keys("90210")
+    session.find_element(*CheckoutPage.btnContinue).click()
 
     # 7. Finish the order
-    session.find_element(By.ID, "finish").click()
+    session.find_element(*OverviewPage.btnFinish).click()
 
     # 8. Check that the order was successful
-    title_complete = session.find_element(By.XPATH, "//*[@data-test='complete-header']").text
+    title_complete = session.find_element(*CompletePage.headerComplete).text
     assert title_complete == "Thank you for your order!"
 
 
@@ -79,41 +79,41 @@ def test_add_remove_item(session):
     session.get(HOST)
 
     # 1. Log into the web store
-    session.find_element(By.ID, "user-name").send_keys(LOGIN)
-    session.find_element(By.ID, "password").send_keys(PASSWORD)
-    session.find_element(By.ID, "login-button").click()
+    session.find_element(*LoginPage.txtLogin).send_keys(LOGIN)
+    session.find_element(*LoginPage.txtPassword).send_keys(PASSWORD)
+    session.find_element(*LoginPage.btnSubmit).click()
 
-    # 2. Add one product to cart
-    elements = session.find_elements(By.CSS_SELECTOR, ".inventory_list .inventory_item")
-    elements[0].find_element(By.XPATH, ".//*[@class='pricebar']//button").click()
+    # 2. Add one product to cart in the Inventory page
+    elements = session.find_elements(*InventoryPage.lstInventory)
+    elements[0].find_element(*InventoryPage.btnAddToCart).click()
 
-    # 3. Verify cart label changed to 1
-    cart = session.find_element(By.ID, "shopping_cart_container")
-    cart_badge = cart.find_element(By.XPATH, ".//*[contains(@class, 'shopping_cart_badge')]")
+    # 3. Verify cart items count label changed to 1
+    cart = session.find_element(*HeaderMenu.btnCart)
+    cart_badge = cart.find_element(*HeaderMenu.cartBadge)
     assert cart_badge.text == '1'
 
     # 4. Open cart
     cart.click()
 
-    # 5. Verify number of items in cart is 1
-    items = session.find_elements(By.CSS_SELECTOR, ".cart_list .cart_item")
+    # 5. Verify number of items in cart page is 1
+    items = session.find_elements(*CartPage.lstCartItems)
     assert len(items) == 1
 
     # 6. Remove item from cart
-    session.find_element(By.XPATH, ".//*[contains(@id, 'remove')]").click()
+    session.find_element(*CartPage.btnRemoveFromCart).click()
 
-    # 7. Verify number of items in cart is 0
-    items = session.find_elements(By.CSS_SELECTOR, ".cart_list .cart_item")
+    # 7. Verify number of items in cart page is 0
+    items = session.find_elements(*CartPage.lstCartItems)
     assert len(items) == 0
 
-    # 8. Go back to products page
-    session.find_element(By.ID, "continue-shopping").click()
+    # 8. Go back to Products page
+    session.find_element(*CartPage.btnContinueShopping).click()
 
-    # 9. Verify cart label changed to none
-    cart = session.find_element(By.ID, "shopping_cart_container")
+    # 9. Verify cart items count label changed to none
+    cart = session.find_element(*HeaderMenu.btnCart)
 
     with pytest.raises(NoSuchElementException):
-        cart.find_element(By.XPATH, ".//*[contains(@class, 'shopping_cart_badge')]")
+        cart.find_element(*HeaderMenu.cartBadge)
 
 
 def test_add_to_cart_thru_product_page(session):
@@ -129,30 +129,30 @@ def test_add_to_cart_thru_product_page(session):
     session.get(HOST)
 
     # 1. Log into the web store
-    session.find_element(By.ID, "user-name").send_keys(LOGIN)
-    session.find_element(By.ID, "password").send_keys(PASSWORD)
-    session.find_element(By.ID, "login-button").click()
+    session.find_element(*LoginPage.txtLogin).send_keys(LOGIN)
+    session.find_element(*LoginPage.txtPassword).send_keys(PASSWORD)
+    session.find_element(*LoginPage.btnSubmit).click()
 
-    # 2. Open first product's page
-    elements = session.find_elements(By.CSS_SELECTOR, ".inventory_list .inventory_item")
-    elements[0].find_element(By.XPATH, ".//*[@id='item_4_title_link']").click()
+    # 2. Open first product's description page
+    elements = session.find_elements(*InventoryPage.lstInventory)
+    elements[0].find_element(*InventoryPage.lnkProductName).click()
 
-    # 3. Open first product's page
-    session.find_element(By.XPATH, ".//*[@id='add-to-cart']").click()
+    # 3. Click Add to cart in the product description page
+    session.find_element(*ProductDescriptionPage.btnAddToCart).click()
 
     # 4. Verify cart label is 1
-    cart = session.find_element(By.ID, "shopping_cart_container")
-    cart_badge = cart.find_element(By.XPATH, ".//*[contains(@class, 'shopping_cart_badge')]")
+    cart = session.find_element(*HeaderMenu.btnCart)
+    cart_badge = cart.find_element(*HeaderMenu.cartBadge)
     assert cart_badge.text == '1'
 
-    # 5. Remove item from cart
-    session.find_element(By.XPATH, ".//*[@id='remove']").click()
+    # 5. Remove item from cart in the product description page
+    session.find_element(*ProductDescriptionPage.btnRemove).click()
 
-    # 6. Verify cart items label is null
-    cart = session.find_element(By.ID, "shopping_cart_container")
+    # 6. Verify cart items count label is null
+    cart = session.find_element(*HeaderMenu.btnCart)
 
     with pytest.raises(NoSuchElementException):
-        cart.find_element(By.XPATH, ".//*[contains(@class, 'shopping_cart_badge')]")
+        cart.find_element(*HeaderMenu.cartBadge)
 
     # 7. Go back to products page
-    session.find_element(By.ID, "back-to-products").click()
+    session.find_element(*ProductDescriptionPage.btnBackToProducts).click()
