36a0acd1858c95480e288b86ae9296ee3053d1f1
t_hrechkina <t_hrechkina-itssv@kw.com>
2024-04-29 10:28:41 +0300

    update ProductPage add ProductDetailsPage classes

--- tests/threc/hw5_saucedemo_oop/page_object/product_page.py
+++ tests/threc/hw5_saucedemo_oop/page_object/product_page.py
@@ -7,28 +7,16 @@ class ProductPage(BasePage):
         products = self.driver.find_elements(*LocProductsPage.listProducts)
         return products
 
-    # def line_product(self, products, index):
-    #     one_product = products[index].find_element(*LocProductsPage.btnAddToCart)
-    #     return one_product
-
-
     def line_product(self, index):
         one_product = self.list_products()[index]
         return one_product
 
-
-    # def add_cart(self, product):
-    #     product.click()
-
-
     def add_to_cart(self, product):
         product.find_element(*LocProductsPage.btnAddToCart).click()
-
 
     def product_label(self, product):
         label = product.find_element(*LocProductsPage.btnProductDetails)
         return label.text
-
 
     def product_details(self, product):
         details = product.find_element(*LocProductsPage.btnProductDetails)
