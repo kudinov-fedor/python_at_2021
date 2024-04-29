0daab3dcf030c8a39559a3b21b442bd4aa262bda
t_hrechkina <t_hrechkina-itssv@kw.com>
2024-04-29 21:21:42 +0300

    add classed for all tests

--- tests/threc/hw5_saucedemo_oop/test_site.py
+++ tests/threc/hw5_saucedemo_oop/test_site.py
@@ -7,7 +7,7 @@ from tests.threc.hw5_saucedemo_oop.page_object.finish_page import FinishPage
 import constants
 
 
-def test_product_details(session):
+def test_product_details(driver):
     """
     Log in to the site
     Find first product
@@ -16,7 +16,7 @@ def test_product_details(session):
     Check opened URL
     Compare product name from Product list page with Product details page
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     products.list_products()
 
     item = products.line_product(0)
@@ -25,14 +25,14 @@ def test_product_details(session):
     product_name = products.product_label(item)
     products.product_details(item)
 
-    details = ProductDetailsPage(session)
+    details = ProductDetailsPage(driver)
     product_details_page = details.product_details_label()
     assert product_name == product_details_page
 
-    assert session.current_url in constants.URL_PRODUCT_PAGE
+    assert driver.current_url in constants.URL_PRODUCT_PAGE
 
 
-def test_add_to_card(session):
+def test_add_to_card(driver):
     """
     Log in to the site
     Check the amount of products on the page
@@ -40,41 +40,41 @@ def test_add_to_card(session):
     Add this product to the cart
     Check the amount of added products to the card by amount on the badge
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     items = products.list_products()
     assert len(items) == constants.PRODUCT_LENGTH
 
     item = products.line_product(0)
     products.add_to_cart(item)
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart_badge = cart.cart_badge_label(cart_item)
 
     assert cart_badge == constants.CART_BADGE
 
 
-def test_add_all_cart(session):
+def test_add_all_cart(driver):
     """
     Log in to the site
     Find all product
     Add all product to the cart
     Check the amount of added products to the cart by amount on the badge
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     products.list_products()
 
     for i in range(constants.PRODUCT_LENGTH):
         item = products.line_product(i)
         products.add_to_cart(item)
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart_badge = cart.cart_badge_label(cart_item)
     assert cart_badge == constants.CART_BADGE_ALL_PRODUCTS
 
 
-def test_navigation_to_cart(session):
+def test_navigation_to_cart(driver):
     """
     Log in to the site
     Find fist product
@@ -82,7 +82,7 @@ def test_navigation_to_cart(session):
     Open cart page
     Compare name of added product from the cart with the name of the product from the products page
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     products.list_products()
 
     item = products.line_product(0)
@@ -92,13 +92,13 @@ def test_navigation_to_cart(session):
 
     assert product_name == constants.PRODUCT_NAME
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart.btn_click(cart_item)
     assert cart.cart_product_name() == product_name
 
 
-def test_continue_shopping(session):
+def test_continue_shopping(driver):
     """
     Log in to the site
     Find first product
@@ -106,41 +106,41 @@ def test_continue_shopping(session):
     Click on the Continue shopping button
     Check the navigation to the Product page
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     products.list_products()
 
     item = products.line_product(0)
     products.add_to_cart(item)
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart.btn_click(cart_item)
     cart.btn_click(cart.cart_continue_btn())
-    assert session.current_url in constants.URL_MAIN_PRODUCT_PAGE
+    assert driver.current_url in constants.URL_MAIN_PRODUCT_PAGE
 
 
-def test_navigate_checkout(session):
+def test_navigate_checkout(driver):
     """
     Log in to the site
     Add first product to the cart
     Click on the Checkout button
     Check that the Checkout step first is opened
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     products.list_products()
 
     item = products.line_product(0)
     products.add_to_cart(item)
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart.btn_click(cart_item)
     cart.btn_click(cart.cart_checkout_btn())
 
-    assert session.current_url in constants.URL_CHECKOUT_STEP_ONE
+    assert driver.current_url in constants.URL_CHECKOUT_STEP_ONE
 
 
-def test_fill_order_form(session):
+def test_fill_order_form(driver):
     """
     Log in to the site
     Add first product to the cart
@@ -151,7 +151,7 @@ def test_fill_order_form(session):
     Click on the Finish button
     Check that the Finish page is opened
     """
-    products = ProductPage(session)
+    products = ProductPage(driver)
     items = products.list_products()
 
     item = products.line_product(0)
@@ -160,20 +160,20 @@ def test_fill_order_form(session):
     product_name = items[0].find_element(*LocProductsPage.btnProductDetails).text
     assert product_name == constants.PRODUCT_NAME
 
-    cart = CartPage(session)
+    cart = CartPage(driver)
     cart_item = cart.shopping_cart_badge()
     cart.btn_click(cart_item)
     cart.btn_click(cart.cart_checkout_btn())
 
-    checkout = CheckoutPage(session)
+    checkout = CheckoutPage(driver)
     checkout.checkout_fill_form(constants.FIRST_NAME, constants.LAST_NAME, constants.ZIPCODE)
     checkout.checkout_btn_click(checkout.checkout_submit_btn())
     checkout_item = checkout.checkout_product_label()
     assert product_name == checkout_item
 
     checkout.checkout_btn_click(checkout.checkout_finish_btn())
-    finish = FinishPage(session)
+    finish = FinishPage(driver)
     finish_title = finish.finish_title()
     assert finish_title == constants.COMPLETE_CHECKOUT
 
-    assert session.current_url in constants.URL_FINISH_CHECKOUT
+    assert driver.current_url in constants.URL_FINISH_CHECKOUT
