12ce4bb47428a1543e4f1a73f7c9f7751d6aef05
fkudinov <kudinov.feodor@gmail.com>
2024-05-02 21:32:58 +0300

    fix android example

--- tests/android_test/test_sample.py
+++ tests/android_test/test_sample.py
@@ -27,7 +27,7 @@ class By(AppiumBy, SeleniumBy):
 
 EXECUTOR = 'http://127.0.0.1:4723/wd/hub'
 ANDROID_BASE_CAPS = {
-    'app': os.path.abspath('/Users/fkudi/PycharmProjects/python_at_2021/tests/android_test/ApiDemos-debug.apk'),
+    'app': os.path.abspath('./ApiDemos-debug.apk'),
     'platformName': 'Android',
     'automationName': 'UiAutomator2',
     "noSign": "true",
