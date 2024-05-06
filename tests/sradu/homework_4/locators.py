d812b11fa68ccd4bd67dfab41579590fedd3219a
Sofiiar <s_radul-itssv@kw.com>
2024-05-06 21:13:01 +0200

    SR-4: updated locators line separation

--- tests/sradu/homework_4/locators.py
+++ tests/sradu/homework_4/locators.py
@@ -10,14 +10,14 @@ class AlertsLocators:
 
     @classmethod
     def get_click_btn_for_item(cls, item_text: str) :
-        xpath = (f"//div[@id='javascriptAlertsWrapper']//span[text()='{item_text}']/ancestor::div[contains(@class, "
-                 f"'row')][1]//button")
+        xpath = (f"//div[@id='javascriptAlertsWrapper']//span[text()='{item_text}']"
+                 f"/ancestor::div[contains(@class,'row')][1]//button")
         return By.XPATH, xpath
 
     @classmethod
     def get_alert_result(cls, item_text: str):
-        xpath = (f"//div[@id='javascriptAlertsWrapper']//span[text()='{item_text}']/following-sibling::span["
-                 f"@id='confirmResult']")
+        xpath = (f"//div[@id='javascriptAlertsWrapper']//span[text()='{item_text}']"
+                 f"/following-sibling::span[@id='confirmResult']")
         return By.XPATH, xpath
 
 
