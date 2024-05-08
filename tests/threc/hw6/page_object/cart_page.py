48502897505881a7378e766b5e4a42dcd3325da4
t_hrechkina <t_hrechkina-itssv@kw.com>
2024-05-08 16:53:12 +0300

    update for hw6

--- tests/threc/hw6/page_object/cart_page.py
+++ tests/threc/hw6/page_object/cart_page.py
@@ -1,9 +1,18 @@
 from tests.threc.hw6.page_object.base_page import BasePage
-from tests.threc.hw6.locators import LocCartPage
+from tests.threc.hw6.page_object.checkout_page import CheckoutPage
+from tests.threc.hw6.locators import LocCartPage, LocCheckoutPage
 
 
 class CartPage(BasePage):
-
     def open_cart(self):
         # open cart page
         return self.click(self.find_element(*LocCartPage.cartLink))
+
+    def click_continue_btn(self):
+        # find button continue and click it
+        self.click(self.find_element(*LocCheckoutPage.btnContinueShopping))
+
+    def click_checkout_btn(self) -> CheckoutPage:
+        # find button checkout and click it
+        self.click(self.find_element(*LocCheckoutPage.btnCheckout))
+        return CheckoutPage(self.driver)
