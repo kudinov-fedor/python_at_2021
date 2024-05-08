e67b82a1b2457e99f0d69dbd695ff8bcb2a5edb5
Yana Pokulita <ypoku@softserveinc.com>
2024-05-08 19:03:06 +0300

    HW6

--- tests/yana_pokulita/HW4_DemoQA/page/login_page.py
+++ tests/yana_pokulita/HW4_DemoQA/page/login_page.py
@@ -1,4 +1,5 @@
 from tests.yana_pokulita.HW4_DemoQA.page.base_page import BasePage
+from tests.yana_pokulita.HW4_DemoQA.page.products_page import ProductsPage
 from tests.yana_pokulita.locators import LoginPageLocators
 from tests.yana_pokulita.HW4_DemoQA.page import constants
 
@@ -7,8 +8,10 @@ class LoginPage(BasePage):
 
     def open(self):
         self.driver.get(constants.HOST)
+        return self
 
     def fill_form(self, login, password):
         self.driver.find_element(*LoginPageLocators.UserName).send_keys(login)
         self.driver.find_element(*LoginPageLocators.Password).send_keys(password)
         self.click(*LoginPageLocators.LoginBtn)
+        return ProductsPage(self.driver)
