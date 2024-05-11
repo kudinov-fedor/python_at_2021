c084ccdb5ed252a2991e42cddec02bb9ddbbc5a8
Yevhen Semenov <eugene.semenoff@gmail.com>
2024-05-12 00:03:41 +0300

    homework 5: fix fill_form(...), submit_form()

--- tests/ysemenov/hw5/pages/login_page.py
+++ tests/ysemenov/hw5/pages/login_page.py
@@ -4,13 +4,13 @@ from tests.ysemenov.hw5.locators import LocatorsLoginPage
 
 class LoginPage(BasePage):
 
-    def fill_login_form(self, login: str, password: str):
+    def fill_form(self, login: str, password: str):
         self.send_keys(*LocatorsLoginPage.TXT_LOGIN, login)
         self.send_keys(*LocatorsLoginPage.TXT_PASSWORD, password)
 
-    def submit_login_form(self):
+    def submit_form(self):
         self.click_by_locator(*LocatorsLoginPage.BTN_SUBMIT)
 
     def log_in(self, login: str, password: str):
-        self.fill_login_form(login, password)
-        self.submit_login_form()
+        self.fill_form(login, password)
+        self.submit_form()
