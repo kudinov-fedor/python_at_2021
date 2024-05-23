5dbc208b2686a6416511423277c24d0221b9ab90
Yevhen Semenov <eugene.semenoff@gmail.com>
2024-05-24 00:14:11 +0300

    homework 7: fix typings

--- tests/ysemenov/hw7/api_client.py
+++ tests/ysemenov/hw7/api_client.py
@@ -1,9 +1,10 @@
 from requests import session
+from tests.ysemenov.hw7.constants import HOST
 
 
 class ApiClient:
 
-    HOST = "https://demoqa.com"
+    HOST = HOST
 
     def __init__(self, login, password):
         self.login = login
@@ -17,7 +18,7 @@ class ApiClient:
         return header.replace("Bearer ", "") if header else None
 
     @token.setter
-    def token(self, token):
+    def token(self, token: str):
         if token:
             self.session.headers["Authorization"] = "Bearer {}".format(token)
         else:
@@ -27,63 +28,63 @@ class ApiClient:
     def token(self):
         self.session.headers.pop("Authorization", None)
 
-    def create_user(self):
+    def create_user(self) -> dict:
         data = {"userName": self.login,
                 "password": self.password}
         res = self.session.post(self.HOST + "/Account/v1/User", json=data)
         res.raise_for_status()
         return res.json()
 
-    def generate_token(self):
+    def generate_token(self) -> dict:
         data = {"userName": self.login,
                 "password": self.password}
-        res = self.session.post(self.HOST + "/Account/v1/GenerateToken",json=data)
+        res = self.session.post(self.HOST + "/Account/v1/GenerateToken", json=data)
         res.raise_for_status()
         return res.json()
 
-    def user_exists(self):
+    def user_exists(self) -> bool:
         data = {"userName": self.login,
                 "password": self.password}
         res = self.session.post(self.HOST + "/Account/v1/Authorized", json=data)
         return res.status_code == 200
 
-    def login_user(self):
+    def login_user(self) -> dict:
         data = {"userName": self.login,
                 "password": self.password}
         res = self.session.post(self.HOST + "/Account/v1/Login", json=data)
         res.raise_for_status()
         return res.json()
 
-    def get_user(self):
+    def get_user(self) -> dict:
         res = self.session.get(self.HOST + "/Account/v1/User/{}".format(self.user_id))
         res.raise_for_status()
         return res.json()
 
-    def get_books(self):
+    def get_books(self) -> dict:
         res = self.session.get(self.HOST + "/BookStore/v1/Books")
         res.raise_for_status()
         return res.json()
 
-    def get_book(self, isbn):
+    def get_book(self, isbn: str) -> dict:
         res = self.session.get(self.HOST + "/BookStore/v1/Book/{}".format(isbn))
         res.raise_for_status()
         return res.json()
 
-    def add_book(self, isbn):
+    def add_book(self, isbn: str) -> dict:
         data = {"userId": self.user_id,
                 "collectionOfIsbns": [{"isbn": isbn}]}
         res = self.session.post(self.HOST + "/BookStore/v1/Books", json=data)
         res.raise_for_status()
         return res.json()
 
-    def replace_book(self, isbn1, isbn2):
+    def replace_book(self, isbn1: str, isbn2: str) -> dict:
         data = {"userId": self.user_id,
                 "isbn": isbn2}
         res = self.session.put(self.HOST + "/BookStore/v1/Books/{}".format(isbn1), json=data)
         res.raise_for_status()
         return res.json()
 
-    def delete_book(self, isbn):
+    def delete_book(self, isbn: str):
         data = {"isbn": isbn,
                 "userId": self.user_id}
         res = self.session.delete(self.HOST + "/BookStore/v1/Book", json=data)
